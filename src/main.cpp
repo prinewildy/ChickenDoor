3b6f12de6a447c755488da4dbfd508a796c858be
prinewildy <b.munro.wild@gmail.com>
2019-04-30 18:28:22 +0100

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "Door.h"
 #include "Stepper.h"
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include "ESP8266WiFi.h"
+#include "ESPAsyncTCP.h"
+#include "ESPAsyncWebServer.h"
+#include "iostream"
 
 const int MOTOR_STEPS = 200;
 
@@ -17,14 +20,14 @@ const int PITCH = 8;
 
 const char *ssid = "duckdog";
 const char *password = "helenandben";
-const char *host = "192.168.0.57";
 
 WiFiServer server(80);
 String header;
 Stepper stepper(DIR, ENABLE, STEP, MOTOR_STEPS);
 Door door(HEIGHT, PITCH, END_STOP, stepper);
 
-void startWiFI() {
+void startWiFI()
+{
   // Connect to WiFi network
   Serial.println();
   Serial.println();
@@ -33,112 +36,89 @@ void startWiFI() {
 
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
 
-  // Start the server
   server.begin();
   Serial.println("Server started");
 
-  // Print the IP address
   Serial.println(WiFi.localIP());
 }
 
-void ICACHE_RAM_ATTR flashISR() {
-  if (door.isDoorMoving() == 1) {
+void ICACHE_RAM_ATTR flashISR()
+{
+  if (door.isDoorMoving() == 1)
+  {
     digitalWrite(LED, !digitalRead(LED));
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
+  AsyncWebServer server(80);
   startWiFI();
   pinMode(LED, OUTPUT);
+  
+
+  server.on("/door/open", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Serial.println("open");
+    if (!door.isDoorMoving())
+    {
+      door.OpenDoor();
+      digitalWrite(LED, LOW);
+    }
+    request->redirect("/door");
+  });
+
+  server.on("/door/close", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Serial.println("close");
+    if (!door.isDoorMoving())
+    {
+      door.CloseDoor();
+      digitalWrite(LED, HIGH);
+    }
+    request->redirect("/door");
+  });
+
+  server.on("/door", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String htmlStr;
+    htmlStr = "<!DOCTYPE html><html>";
+    htmlStr = htmlStr + "<head><meta name=\"viewport\" "
+                        "content=\"width=device-width, initial-scale=1\">";
+    htmlStr = htmlStr + "<link rel=\"icon\" href=\"data:,\">";
+    htmlStr = htmlStr + "<style>html { font-family: Helvetica; display: "
+                        "inline-block; margin: 0px auto; text-align: "
+                        "center;}";
+    htmlStr = htmlStr + ".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;";
+    htmlStr = htmlStr + "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}";
+    htmlStr = htmlStr + ".button2 {background-color: #77878A;}</style></head>";
+    htmlStr = htmlStr + "<body><h1>Chicken Door Web Server</h1>";
+    if (door.getDoorState() == 0)
+    {
+      htmlStr = htmlStr + "<p>Door State - Closed</p>";
+      htmlStr = htmlStr + "<p><a href=\"/door/open\"><button class=\"button\">Open</button></a></p>";
+    }
+    else
+    {
+      htmlStr = htmlStr + "<p>Door State - Open</p>";
+      htmlStr = htmlStr + "<p><a href=\"/door/close\"><button class=\"button button2\">Close</button></a></p>";
+    }
+    htmlStr = htmlStr + "</body></html>";
+    request->send(200, "text/plain", htmlStr);
+  });
+
   timer1_isr_init();
   timer1_attachInterrupt(flashISR);
   timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP);
   timer1_write(75000);
 }
 
-void loop() {
-
-  WiFiClient client = server.available();
-
-  if (client) {
-    Serial.println("New Client.");
-    String currentLine = "";
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        header += c;
-
-        if (c == '\n') {
-
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            if (header.indexOf("GET /door/open") >= 0) {
-              Serial.println("open");
-              door.OpenDoor();
-              digitalWrite(LED, LOW);
-            } else if (header.indexOf("GET /door/close") >= 0) {
-              Serial.println("close");
-              door.CloseDoor();
-              digitalWrite(LED, HIGH);
-            }
-
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" "
-                           "content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-
-            client.println("<style>html { font-family: Helvetica; display: "
-                           "inline-block; margin: 0px auto; text-align: "
-                           "center;}");
-            client.println(".button { background-color: #195B6A; border: none; "
-                           "color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: "
-                           "2px; cursor: pointer;}");
-            client.println(
-                ".button2 {background-color: #77878A;}</style></head>");
-
-            client.println("<body><h1>Chicken Door Web Server</h1>");
-
-            if (door.getDoorState() == 0) {
-              client.println("<p>Door State - Closed</p>");
-              client.println("<p><a href=\"/door/open\"><button "
-                             "class=\"button\">Open</button></a></p>");
-            } else {
-              client.println("<p>Door State - Open</p>");
-              client.println("<p><a href=\"/door/close\"><button "
-                             "class=\"button button2\">Close</button></a></p>");
-            }
-
-            client.println("</body></html>");
-
-            client.println();
-
-            break;
-          } else {
-            currentLine = "";
-          }
-        } else if (c != '\r') {
-          currentLine += c;
-        }
-      }
-    }
-
-    header = "";
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+void loop()
+{
 }
